a38c6ab89bb8cba019ee377af9029c0cf4dfbfb5
nununu2610 <nnhu7732@gmail.com>
2025-05-09 14:30:23 +0700

    Sửa giao diện

--- UNO/App.xaml.cs
+++ UNO/App.xaml.cs
@@ -2,12 +2,19 @@
 using System.Data;
 using System.Windows;
 
-namespace UNO;
-
-/// <summary>
-/// Interaction logic for App.xaml
-/// </summary>
-public partial class App : Application
+namespace UNO
 {
-}
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
 
+            this.ShutdownMode = ShutdownMode.OnExplicitShutdown;
+
+            var menu = new Views.Menu();
+            this.MainWindow = menu; // giữ tham chiếu chính
+            menu.Show();
+        }
+    }
+}
